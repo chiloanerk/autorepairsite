1d75d20c44a16e66de492bdad07ce31155d1b5c1
chiloanerk <chiloanerk@gmail.com>
2024-02-01 20:17:07 +0200

    Minor typo fix

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -3,8 +3,8 @@ function Footer() {
         <footer id="footer_section" className="footer_section">
             <div className="container">
                 <p>
-                    &copy; <span id="displayYear"></span> All Rights Reserved By
-                    <a href="https://githhub.com/chiloanerk">Website By RK Chiloane</a>
+                    &copy; <span id="displayYear"></span> All Rights Reserved -
+                    <a href="https://githhub.com/chiloanerk"> chiloanerk</a>
                 </p>
             </div>
         </footer>
